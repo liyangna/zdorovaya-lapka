bb55d30988a1e5e58613c2900c87ac88e4aa5184
liyangna <160404355+liyangna@users.noreply.github.com>
2026-05-25 18:54:17 +0300

    Add files via upload

--- server.js
+++ server.js
@@ -13,8 +13,6 @@ const app = express();
 app.use(express.json());
 app.use(express.static('.'));
 
-const SHOP_ID = '1367464';
-const SECRET_KEY = 'test_EN-raAsqyqfDne6g3pGfkqahT_q_J9ebQ0RBhgIhz9k';
 
 app.post('/api/create-payment', async (req, res) => {
     const { amount, description } = req.body;
